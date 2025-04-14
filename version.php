e0b3f06a9d6231e9003e1eb071627829249d9f5d
benyovszky <molbence@gmail.com>
2025-04-14 08:07:14 +0200

    Typo2

--- version.php
+++ version.php
@@ -26,7 +26,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'profilefield_textregex'; // Full name of the plugin (used for diagnostics).
-$plugin->version   = 202502l41400;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2025041400;        // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2021051718;        // Requires this Moodle version.
 $plugin->maturity = MATURITY_STABLE;
 $plugin->release = '1.0.1';
